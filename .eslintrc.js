5cb27211703d22e42210adaf561db94fb6918f99
qq491476277 <491476277@qq.com>
2020-12-28 17:59:19 +0800

    fix: 增加eslint插件和配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,13 @@
+const OFF = 0;
+const ERROR = 2;
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+    env: {
+        browser: true,
+        es2020: true,
+        node: true,
     },
-    
-    "extends": [
+
+    extends: [
         'airbnb',
         'airbnb/hooks',
         'plugin:eslint-comments/recommended',
@@ -14,37 +16,33 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:unicorn/recommended',
         'prettier',
-        // 专门支持了 eslint-plugin-react
         'prettier/react',
-        // 专门支持了 @typescript-eslint/eslint-plugin
         'prettier/@typescript-eslint',
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
         },
-        "ecmaVersion": 12,
-        "sourceType": "module"
+        ecmaVersion: 12,
+        sourceType: 'module',
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
         'no-useless-constructor': 'off',
         '@typescript-eslint/no-useless-constructor': 'error',
+        'import/extensions': [
+            ERROR,
+            'ignorePackages',
+            {
+                ts: 'never',
+                tsx: 'never',
+                json: 'never',
+                js: 'never',
+            },
+        ],
     },
-    'import/extensions': [
-        '2',
-        'ignorePackages',
-        {
-            ts: 'never',
-            tsx: 'never',
-            json: 'never',
-            js: 'never'
-        },
-    ],
+
     settings: {
         'import/resolver': {
             node: {
@@ -55,8 +53,7 @@ module.exports = {
                 // 配置 eslint-import-resolver-typescript 读取 tsconfig.json 的路径
                 // 目前用不着，先注释掉
                 // directory: [resolve('./src/tsconfig.json'), resolve('./scripts/tsconfig.json')],
-
-            }
+            },
         },
     },
     overrides: [
@@ -73,6 +70,4 @@ module.exports = {
             },
         },
     ],
-
-
 };
